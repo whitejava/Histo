e498cfce3818f50cac1456cfa147385f783e08d2
caipeichao <PC@PC-PC>
2012-06-11 02:16:19 +0800

    整理cipher.aes.performance_test

--- Histo/cipher/aes/_performance_test.py
+++ Histo/cipher/aes/_performance_test.py
@@ -3,9 +3,10 @@ from .cipher import cipher
 from ._timer import timer
 
 #2012 06 09 0.20145043181969796
+#2012 06 11 0.20845242799156374
 class test(unittest.TestCase):
     def test_encrypt(self):
-        key = b'0'*32
-        c = cipher(key)
-        with timer():
-            c.encrypt(b'1'*10*1024*1024+b'2')
+        c = cipher(bytes(32))
+        for _ in range(10):
+            with timer():
+                c.encrypt(b'1'*10*1024*1024+b'2')
