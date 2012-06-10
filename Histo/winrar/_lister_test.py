44b06f1eba096c0fb8664c07abae4b5a413088a9
caipeichao <PC@PC-PC>
2012-06-10 12:22:38 +0800

    finish winrar

--- Histo/winrar/_lister_test.py
+++ Histo/winrar/_lister_test.py
@@ -3,7 +3,7 @@ from .lister import lister
 
 class test(unittest.TestCase):
     def test_normal(self):
-        expect = ['1.txt','2','2/3.txt']
+        expect = ['2','1.txt','2\\3.txt']
         self.assertEquals(expect, self._list('normal'))
     
     def test_embed(self):
@@ -11,15 +11,15 @@ class test(unittest.TestCase):
         self.assertEquals(expect, self._list('embed'))
     
     def test_part_unlistable(self):
-        expect = ['1.txt','2','2/3.rar']
+        expect = ['2','1.txt','2\\3.rar']
         self.assertEquals(expect, self._list('part_unlistable'))
         
     def test_unlistable(self):
-        expect = ['?']
+        expect = []
         self.assertEquals(expect, self._list('unlistable'))
         
     def test_encrypt(self):
-        expect = ['?']
+        expect = []
         self.assertEquals(expect, self._list('encrypt'))
     
     def test_part_encrypt(self):
@@ -27,8 +27,15 @@ class test(unittest.TestCase):
         self.assertEquals(expect, self._list('part_encrypt'))
     
     def test_hidden(self):
-        expect = ['1.jpg','1.jpg>>2.txt']
+        expect = ['1.jpg','2.jpg'] # 2.jpg contains hidden file, but cannot detect.
         self.assertEquals(expect, self._list('hidden'))
+
+    def test_list_dir(self):
+        import os
+        expect = ['embed','empty','embed\\a','embed\\file','embed\\a\\file']
+        path = os.path.join(os.path.dirname(__file__), '_dir_test')
+        li = lister._list_dir(None, path)
+        self.assertEquals(expect,li)
     
     def _list(self,x):
         import os
--- Histo/winrar/lister.py
+++ Histo/winrar/lister.py
@@ -4,7 +4,8 @@ import os
 class lister:
     def __init__(self,rar):
         self._rar = rar
-    
+        self._archive_extensions = 'RAR,ZIP,7Z,ACE,ARJ,BZ2,CAB,GZ,ISO,JAR,LZH,TAR,UUE,Z'.split(',')
+
     def list(self):
         r = []
         with tempdir(prefix='rarlist') as d:
@@ -12,21 +13,30 @@ class lister:
                 li = self._list_dir(d)
                 for e in li:
                     r.append(e)
-                    if self._is_file(d,e):
-                        for e2 in lister(e).list():
-                            r.append('>>'.join(e,e2))
+                    f = os.path.join(d,e)
+                    if self._is_archive_file(f):
+                        for e2 in lister(f).list():
+                            r.append('>>'.join([e,e2]))
+        return r
     
     def _list_dir(self,d):
         r = []
-        for dirname, dirnames, filenames in os.walk('.'):
+        for dirname, dirnames, filenames in os.walk(d):
             for e in dirnames:
-                r.append(e[len(d)+1:])
+                x=os.path.join(dirname,e)
+                r.append(x[len(d)+1:])
             for e in filenames:
-                r.append(e[len(d)+1:])
+                x=os.path.join(dirname,e)
+                r.append(x[len(d)+1:])
         return r
     
-    def _is_file(self,root,file):
-        return os.path.isfile(os.path.join(root,file))
+    def _extract_rar(self,rar,d):
+        import subprocess
+        subprocess.call(['winrar','x',rar,'-ibck','-inul',d+'\\'])
+        return True
     
-    def _extract_dir(self,d):
-        'winrar e {} -ibck'
+    def _is_archive_file(self,f):
+        if not os.path.isfile(f):
+            return False
+        ext = f.split('.')[-1].upper()
+        return ext in self._archive_extensions
