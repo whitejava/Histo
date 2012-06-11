a798db42494201e3cf18e982d5702e0d828c3351
caipeichao <PC@PC-PC>
2012-06-11 17:09:25 +0800

    update dfile.bundle.broken

--- Histo/dfile/bundle/broken/_bundle_test.py
+++ Histo/dfile/bundle/broken/_bundle_test.py
@@ -1,14 +1,86 @@
 import unittest
-from ..broken.bundle import bundle as broken
-from ..memory.bundle import bundle
 
 class test(unittest.TestCase):
-    def test_normal(self):
-        b = broken(bundle(), [1])
-        b.dump(0,b'123')
-        assert b.load(0) == b'123'
+    def test_no_broken(self):
+        self._broken([])
+        self._good_dump(0)
+        
+    def test_avoid_broken(self):
+        self._broken([1])
+        self._good_dump(0)
     
-    def test_broken(self):
-        b = broken(bundle(),[1])
-        with self.assertRaises(Exception):
-            b.dump(1,b'123')
+    def test_one_broken(self):
+        self._broken([1])
+        self._bad_dump(1, 'target is broken')
+    
+    def test_multi_broken(self):
+        self._broken([0,1,2])
+        for i in range(3):
+            self._bad_dump(i, 'target is broken')
+    
+    def test_broken_types(self):
+        self._broken((1,2))
+        self._broken([1,2])
+        self._broken(range(3))
+    
+    def test_normal_exists(self):
+        self._broken([1])
+        self._good_exists(0, False)
+    
+    def test_broken_exists(self):
+        self._broken([1])
+        self._bad_exists(1, 'target is broken')
+    
+    def test_broken_load(self):
+        self._broken([1])
+        self._bad_load(1, 'target is broken')
+    
+    def _broken(self, li):
+        from .bundle import bundle as broken
+        from ..memory.bundle import bundle
+        self._bundle = broken(bundle(), li)
+    
+    def _good_dump(self, n, data = b'123'):
+        self._dump(n, data)
+        self._load(n, data)
+    
+    def _bad_dump(self, n, message, data = b'123'):
+        try:
+            self._dump(n, data)
+        except Exception as e:
+            self.assertEquals(message, e.args[0])
+        else:
+            self.fail('expect exception')
+    
+    def _dump(self, n, data):
+        self._bundle.dump(n, data)
+    
+    def _load(self, n, expect):
+        self.assertEquals(expect, self._bundle.load(n))
+        
+    def _good_exists(self, n, expect):
+        self.assertEquals(expect, self._bundle.exists(n))
+    
+    def _bad_exists(self, n, message):
+        try:
+            self._bundle.exists(n)
+        except Exception as e:
+            self.assertEquals(e.args[0], message)
+        else:
+            self.fail('expect exception')
+    
+    def _bad_broken(self, li, message):
+        try:
+            self._broken(li)
+        except Exception as e:
+            self.assertEquals(message, e.args[0])
+        else:
+            self.fail('expect exception')
+            
+    def _bad_load(self, n, message):
+        try:
+            self._bundle.load(n)
+        except Exception as e:
+            self.assertEquals(message, e.args[0])
+        else:
+            self.fail('expect exception')
--- Histo/dfile/bundle/broken/bundle.py
+++ Histo/dfile/bundle/broken/bundle.py
@@ -3,19 +3,18 @@ class bundle:
         self._base = base
         self._broken = broken
     
-    def dump(self,n,b):
+    def dump(self, n, data):
         self._ensure_not_broken(n)
-        return self._base.dump(n,b)
+        return self._base.dump(n,data)
     
-    def load(self,n):
+    def load(self, n):
         self._ensure_not_broken(n)
         return self._base.load(n)
     
-    def exists(self,n):
+    def exists(self, n):
         self._ensure_not_broken(n)
         return self._base.exists(n)
     
-    def _ensure_not_broken(self,n):
-        for e in self._broken:
-            if e == n:
-                raise IOError('broken')
+    def _ensure_not_broken(self, n):
+        if n in self._broken:
+            raise Exception('target is broken')
