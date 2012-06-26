383221aef47b3f50291bad8974e624b971f52282
caipeichao <346089341@qq.com>
2012-06-26 16:24:52 +0800

    - Update performance test. - Run performance test.

--- Histo/cipher/aes/_test_performance.py
+++ Histo/cipher/aes/_test_performance.py
@@ -2,24 +2,19 @@ import unittest
 from .cipher import cipher
 from timer import timer
 
-#2012 06 09 0.20145043181969796 win7
-#2012 06 11 0.20845242799156374 win7
-#2012 06 11 0.19000000000000003 ubuntu1204/vmware
-#2012 06 26 0.32000000000000030 ubuntu1204/vmware
-#2012 06 26 0.16999999999999993 ubuntu1204/vmware
+#2012 06 09 0.20145043181969796 win7/Core i5
+#2012 06 11 0.20845242799156374 win7/Core i5
+#2012 06 11 0.19000000000000003 ubuntu1204/vmware/win7/Core i5
+#2012 06 26 0.32000000000000030 ubuntu1204/vmware/win7/70% Core i5
+#2012 06 26 0.16999999999999993 ubuntu1204/vmware/win7/Core i5
+#2012 06 26 0.15000000000000013 ubuntu1204/vmware/win7/Core i5
 class test(unittest.TestCase):
     def test_encrypt(self):
-        #Key
-        key = bytes(32)
         #Cipher
-        c = cipher(key)
+        c = cipher(bytes(32))
         #Several times
         for _ in range(10):
             #Timer
             with timer():
-                #Data size
-                datasize = 10*1024*1024
-                #Data
-                data = b'1'*datasize+b'2'
-                #Encrypt
-                c.encrypt(data)
+                #Encrypt regular data.
+                c.encode(b'1'*(10*1024*1024+1))
