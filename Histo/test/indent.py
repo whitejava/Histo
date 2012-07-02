f6951bb73c0204beb61ad8f5ff751ca04ff7abbd
caipeichao <PC@PC-PC>
2012-07-02 19:49:01 +0800

    - Degrade package: indent. - Move indent.test into test package. - Add __all__ variable in indent

---
+++ Histo/test/indent.py
@@ -0,0 +1,42 @@
+from unittest import TestCase
+from indent import indent
+
+result0 =\
+'''(
+    1,
+    (
+    ),
+    'asdf',
+    (
+        'a',
+        'b',
+    ),
+    (
+        2,
+        3,
+    ),
+    (
+        4,
+        (
+            5,
+            6,
+            7,
+        ),
+    ),
+)'''
+
+class test(TestCase):
+    def test(self):
+        self._input = (1, (), 'asdf', ['a','b'], (2,3),(4,(5,6,7)))
+        self._indent()
+        self._print_output()
+        self._expect(result0)
+    
+    def _indent(self):
+        self._output = indent(self._input)
+    
+    def _expect(self, v):
+        self.assertEquals(v, self._output)
+    
+    def _print_output(self):
+        print(self._output)
