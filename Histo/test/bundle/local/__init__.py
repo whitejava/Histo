ac078d2f3d3872e069ee405b5ada751f2024dac0
caipeichao <PC@PC-PC>
2012-07-02 15:50:08 +0800

    Strenthen test: bundle.local.__init__

--- Histo/test/bundle/local/__init__.py
+++ Histo/test/bundle/local/__init__.py
@@ -1,23 +1,28 @@
-from pctest import testcase,dumpdir,runins
+from pctest import testcase,dumpdir,runins,createfiles
 from bundle import local
 from autotemp import tempdir
 import hex
+import os
 
 class test(testcase):
     def test(self):
         self.bulktest(data, func)
 
-def func(*k):
+def func(subdir, *k):
+    subdir = eval(subdir)
     with tempdir() as temp:
+        temp = os.path.join(temp, *subdir)
         bundle = local(temp, '{:04}')
         t = {'dump':lambda x,y:bundle.dump(eval(x),hex.decode(y)),
-             'load':lambda x:bundle.load(eval(x)),
+             'load':lambda x:hex.encode(bundle.load(eval(x))),
              'exists':lambda x:bundle.exists(eval(x)),
-             'dumpdir':lambda:dumpdir(temp)}
+             'dumpdir':lambda:dumpdir(temp),
+             'createfiles': lambda x:createfiles(temp,x)}
         return runins(t,k)
 
 data = \
 '''
+[]
 dump 0 aabb
 load 0
 exists 0
@@ -35,5 +40,18 @@ exists 2
 load 1
 load 2
 dumpdir
-b'\\xaa\\xbb'-True-False-True-b'\\xaa\\xbb'-b'\\xcc\\xdd'-True-b'\\xbb\\xcc'-True-False-b'\\xdd\\xcc'-IOError(2, 'No such file or directory')-0000:bbcc,0001:ddcc
+aabb-True-False-True-aabb-ccdd-True-bbcc-True-False-ddcc-IOError(2, 'No such file or directory')-0000:bbcc,0001:ddcc
+
+['nonexists']
+dump 0 aabb
+load 0
+?
+
+[]
+createfiles 0000:<folder>
+exists 0
+dump 0 aabb
+load 0
+exists 0
+False-IOError(13, 'Permission denied')-IOError(13, 'Permission denied')-False
 '''
