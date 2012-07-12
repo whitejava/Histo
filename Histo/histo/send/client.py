dad079e4a6065e1eeb1f979ee178846197684f53
caipeichao <PC@PC-PC>
2012-07-12 16:43:38 +0800

    Delete old code.

--- Histo/histo/send/client.py
+++
@@ -1,9 +0,0 @@
-from stream import tcpstream, objectstream
-
-def addqueue(address, x):
-    stream = tcpstream(address)
-    stream = objectstream(stream)
-    stream.writeobject('add')
-    stream.writeobject(x)
-    assert stream.readobject() == 'ok'
-    stream.close()
