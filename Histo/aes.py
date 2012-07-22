ac1feb460996422497521f1305bb3b25de8ecf57
caipeichao <346089341@qq.com>
2012-07-22 13:15:42 +0800

    Simplify aes

--- Histo/aes.py
+++ Histo/aes.py
@@ -1,6 +1,5 @@
 from Crypto.Cipher import AES
-from Crypto.Random import random
-import pkcs7padding
+import pkcs7padding, os
 
 __all__ = ['cipher']
 
@@ -10,24 +9,15 @@ def _encode(key, iv, data):
 def _decode(key, iv, data):
     return AES.new(key, AES.MODE_CBC, iv).decrypt(data)
 
-def _randomiv(length):
-    #Random bytes in specified length.
-    return bytes([random.randrange(256)for _ in range(length)])
-
 class cipher:
     def __init__(self, key):
         self._key = key
     
     def encode(self, data):
-        #Random iv
-        iv = _randomiv(AES.block_size)
-        #Encode
+        iv = os.urandom(AES.block_size)
         data = _encode(self._key, iv, pkcs7padding.encode(data))
-        #Wrap iv
         return iv + data
     
     def decode(self, data):
-        #Unwrap iv
         iv, data = data[:AES.block_size], data[AES.block_size:]
-        #Decode
         return pkcs7padding.decode(_decode(self._key, iv, data))
