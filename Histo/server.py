37c2ff447ce4024d6979a1b2afc1963078826123
caipeichao <346089341@qq.com>
2012-07-23 10:36:02 +0800

    Simplify

--- Histo/server.py
+++ Histo/server.py
@@ -1,18 +1,9 @@
-from stream import copy, objectstream
-from summary import generatesummary
-from netserver import netserver
-from timetuple import nowtuple, totuple
-from autotemp import tempdir
-from repo import repo
-from datetime import datetime
-from taskqueue import diskqueue,taskqueue, NoTask
-from filelock import filelock
-from threading import Thread
-import hashlib, pchex, threading, summary, tempfile
-import time, smtp, os, io, sys, logging, shutil
-import pclib
+import threading, summary, tempfile
+import os, io, sys, logging, shutil
+import pclib, repo
 from smtp import smtpserver
-from pclib import copystream, objectstream
+from pclib import copystream, objectstream, byteshex, netserver, nowtuple
+from summary import generatesummary
 
 logpath = 'E:\\histo-log\\0.log'
 logdateformat = '[%Y-%m%d %H:%M:%S]'
@@ -35,7 +26,7 @@ logging.getLogger().addHandler(handler)
 # server root key threadcount
 
 def main(root, key, threadcount = '5'):
-    key = pchex.decode(key)
+    key = byteshex.decode(key)
     threadcount = int(threadcount)
     
     logging.debug('Loading smtp server')
@@ -103,7 +94,7 @@ class mainserver(netserver):
         logging.debug('writing data')
         data = self._repo.open('data', 'wb')
         start = data.tell()
-        with open(path, 'rb') as f:
+        with open(package, 'rb') as f:
             copystream(f, data)
         end = data.tell()
         
@@ -133,11 +124,42 @@ class mainserver(netserver):
         return result
     
     def get(self, stream):
-        pass
+        start, end = stream.readobject()
+        f = self._repo.open('data', 'rb')
+        missing = f.getmissingpart(start, end)
+        if missing:
+            stream.writeobject('missing')
+            stream.writeobject(missing)
+        else:
+            stream.writeobject('data')
+            copystream(f, stream, end-start)
+        f.close()
     
 def pack(self, name, path, compress):
     root = 'G:\\'
-    raise Exception()
+    time = '%04d-%02d-%02d-%02d-%02d-%02d' % nowtuple()[:6]
+    compress = {True: '-m5', False:'-m0'}[compress]
+    package = os.path.join(root, '%s-%s' % (time, name))
+    
+    files = os.listdir(path)
+    if not files:
+        files = ['Empty']
+        with open(os.path.join(path, 'Empty'), 'wb'): pass
+    else:
+        files = '\n'.join(files) + '\n'
+        files = bytes(files, 'utf16')
+        listfile = tempfile.NamedTemporaryFile(mode = 'wb', delete = False)
+        listfile.write(files)
+        listfile.close()
+    
+    cwd = os.getcwd()
+    os.chdir(path)
+    assert 0 == pclib.quietcall('rar a "%s" @"%s" -scul %s' % (package, listfile.name, compress))
+    os.chdir(cwd)
+    
+    os.unlink(listfile.name)
+    
+    return package
 
 def loaditem(x):
     return dict(x)
@@ -154,223 +176,17 @@ def loadindex(repo):
         return []
     missing = f.getmissingparts()
     if missing:
-        raise Exception('Missing parts: ' + ' '.join([str(e) for e in missing]))
-    stream = f.read()
-    f.close()
-    stream = objectstream(io.BytesIO(stream))
-    result = []
-    while True:
-        try:
-            result.append(indexitem(stream.readobject()))
-        except EOFError:
-            break
-    return result
-
-def indexitem(x):
-    return dict(x)
-
-class mainserver(netserver):
-    def __init__(self, repo):
-        netserver.__init__(self, ('127.0.0.1', 13750), self.handle)
-        self._repo = repo
-        self._index = loadindex(repo)
-        self._lock = threading.Lock()
-    
-    def handle(self, stream):
-        method = stream.readobject()
-        logging.debug('request: ' + method)
-        t = {'remotecommit': self._remotecommit,
-             'localcommit': self._localcommit,
-             'search': self._search,
-             'get': self._get,
-             'commitunpack': self._commitunpack,
-             'upload': self._upload}
-        with self._lock: #WARNING: Careful remove the lock. Thinking about multithread situation
-            try:
-                t[method](stream)
-            except Exception as e:
-                logging.exception(e)
-
-    def _remotecommit(self, stream):
-        datetime = stream.readobject()
-        name = stream.readobject()
-        lastmodify = stream.readobject()
-        filename = stream.readobject()
-        filesize = stream.readobject()
-        logging.debug('name: ' + name)
-        logging.debug('filesize: {}'.format(filesize))
-        logging.debug('receiving data')
-        with tempdir('histo-repo-') as t:
-            temp = os.path.join(t, filename)
-            with open(temp, 'wb') as f:
-                assert copy(stream, f, filesize) == filesize
-            logging.debug('receive data ok')
-            logging.debug('writting to repo')
-            self._commitpack(datetime, name, lastmodify, temp)
-            logging.debug('write ok')
-        stream.writeobject('ok')
-        logging.debug('all ok')
-
-    def _localcommit(self, stream):
-        time = stream.readobject()
-        if time == None:
-            time = nowtuple()
-        name = stream.readobject()
-        filename = stream.readobject()
-        lastmodify = os.path.getmtime(filename)
-        logging.debug('localcommit: ' + filename)
-        logging.debug('writing to repo')
-        self._commitpack(time, name, lastmodify, filename)
-        logging.debug('write ok')
-        stream.writeobject('ok')
-        logging.debug('all ok')
-    
-    def _commitpack(self, datetime, name, lastmodify, filename):
-        repo = self._repo
-        datafile = repo.open('data', 'wb')
-        start = datafile.tell()
-        with open(filename, 'rb') as f:
-            copy(f, datafile)
-        end = datafile.tell()
-        logging.debug('generating summary')
-        summary = generatesummary(name, filename, depthlimit = 2)
-        index = (('datetime', datetime),
-                 ('name', name),
-                 ('last-modify', lastmodify),
-                 ('range', (start, end)),
-                 ('summary', summary))
-        logging.debug('writing index')
-        indexfile = repo.open('index', 'wb')
-        objectstream(indexfile).writeobject(index)
-        datafile.close()
-        indexfile.close()
-        self._index.append(indexitem(index))
-
-    def _commitunpack(self, stream):
-        path = stream.readobject()
-        compress = stream.readobject()
-        datetime = nowtuple()
-        name = os.path.basename(path)
-        name = name + '.rar'
-        logging.debug('name: ' + name)
-        lastmodify = os.path.getmtime(path)
-        path2 = path + '-committing'
-        shutil.move(path, path2)
-        logging.debug('generating summary')
-        summary = generatesummary(name, path2, depthlimit = 2)
-        logging.debug('packing')
-        archive = self._pack(compress, path2)
-        logging.debug('writing data')
-        datafile = self._repo.open('data', 'wb')
-        start = datafile.tell()
-        with open(archive, 'rb') as f:
-            copy(f, datafile)
-        end = datafile.tell()
-        index = (('datetime', datetime),
-                 ('name', name),
-                 ('last-modify', lastmodify),
-                 ('range', (start, end)),
-                 ('summary', summary))
-        logging.debug('writing index')
-        indexfile = self._repo.open('index', 'wb')
-        objectstream(indexfile).writeobject(index)
-        datafile.close()
-        indexfile.close()
-        shutil.rmtree(path2)
-        self._index.append(indexitem(index))
-        logging.debug('write ok')
-        stream.writeobject('ok')
-        
-    def _pack(self, compress, directory):
-        archiveroot = 'G:\\'
-        
-        #Generate current time
-        time = datetime.now()
-        time = time.timetuple()
-        time = time[:6]
-        time = '{:04d}-{:02d}-{:02d}-{:02d}-{:02d}-{:02d}'.format(*time)
-    
-        #Handle compression paramter
-        t = {False:'-m0', True:'-m5'}
-        compression = t[compress]
-        
-        #Get archive name
-        name = os.path.basename(directory)
-        
-        #Get archive full path
-        archive = '{}-{}.rar'.format(time, name)
-        archive = os.path.join(archiveroot, archive)
-        
-        #Generate file list
-        files = os.listdir(directory)
-        if not files:
-            files = ['Empty']
-            with open(os.path.join(directory, 'Empty'),'wb'): pass
-        files = '\n'.join(files) + '\n'
-        files = bytes(files, 'utf16')
-        
-        #Create listfile
-        listfile = tempfile.NamedTemporaryFile(mode = 'wb', delete = False)
-        listfile.write(files)
-        listfile.close()
-        
-        #Change working directory for rar
-        os.chdir(directory)
-        
-        #Call rar to compress
-        command = ['rar',
-                   'a', #Create archive file
-                   '"{}"'.format(archive), #Archive path
-                   '@"{}"'.format(listfile.name), #List file
-                   '-scul', #List file is utf16
-                   compression, #compression
-                   '-w"{}"'.format(directory)] #Set working directory
-        command = ' '.join(command)
-        assert pclib.quietcall(command) == 0
-        
-        #Release directory
-        os.chdir(archiveroot)
-        
-        #Clean up
-        os.remove(listfile.name)
-        
-        #Return
-        return archive
-
-    def _search(self, stream):
-        keyword = stream.readobject()
-        result = []
-        for e in self._index:
-            for e2 in summary.walk(e['summary']):
-                if e2.find(keyword) >= 0:
-                    result.append(e)
-                    break
-        stream.writeobject(result)
-
-    def _get(self, stream):
-        range = stream.readobject()
-        f = self._repo.open('data', 'rb')
-        missing = f.getmissingparts(range[0], range[1])
-        if missing:
-            stream.writeobject('missing')
-            stream.writeobject(missing)
-        else:
-            stream.writeobject('data')
-            f.seek(range[0])
-            copy(f, stream, range[1] - range[0])
+        raise Exception('Missing: ' + ' '.join([str(e) for e in missing]))
+    else:
+        stream = objectstream(io.BytesIO(f.read()))
         f.close()
-
-    def _upload(self, stream):
-        type = stream.readobject()
-        filename = stream.readobject()
-        data = stream.readobject()
-        try:
-            self._repo.add_raw(type, filename, data)
-        except ValueError as e:
-            if e.args[0] == 'file existed':
-                stream.writeobject('fail')
-        else:
-            stream.writeobject('ok')
+        result = []
+        while True:
+            try:
+                result.append(loaditem(stream.readobject()))
+            except EOFError:
+                break
+        return result
 
 if __name__ == '__main__':
     main(*sys.argv[1:])
