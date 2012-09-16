97cc886aa5563c8dcaed36ce7221ab45c4a4b485
caipeichao <346089341@qq.com>
2012-09-16 22:13:52 +0800

    Finish bundle.crypto

---
+++ Histo/histo/bundle/crypto.py
@@ -0,0 +1,65 @@
+class Crypto:
+    def __init__(self, bundle, cipherFactory):
+        self.bundle = bundle
+        self.cipherFactory = cipherFactory
+    
+    def open(self, name, mode):
+        file = self.bundle.open(name, mode)
+        if mode == 'wb':
+            return CryptoWriter(file, self.cipherFactory.encrypt())
+        elif mode == 'rb':
+            return CryptoReader(file, self.cipherFactory.decrypt())
+        else:
+            raise Exception('No such mode.')
+    
+    def delete(self, name):
+        return self.bundle.delete(name)
+    
+    def list(self):
+        return self.bundle.list()
+
+class CryptoWriter:
+    def __init__(self, file, cipher):
+        self.file = file
+        self.cipher = cipher
+    
+    def write(self, data):
+        self.file.write(self.cipher.update(data))
+    
+    def close(self):
+        self.file.write(self.cipher.final())
+        self.file.close()
+
+class CryptoReader:
+    def __init__(self, file, cipher):
+        self.file = file
+        self.cipher = cipher
+        self.buffer = b''
+        self.generator = self.decryptionGenerator()
+    
+    def read(self, limit):
+        result = self.read2(limit)
+        self.buffer = result[limit:]
+        return result[:limit]
+    
+    def close(self):
+        self.file.close()
+
+    def decryptionGenerator(self):
+        trunkSize = 4*1024
+        while True:
+            read = self.file.read(trunkSize)
+            if not read:
+                break
+            yield self.cipher.update(read)
+        yield self.cipher.final()
+    
+    def read2(self, limit):
+        import io
+        result = io.BytesIO(self.buffer)
+        try:
+            while result.tell() < limit:
+                result.write(next(self.generator))
+        except StopIteration:
+            pass
+        return result.getvalue()
