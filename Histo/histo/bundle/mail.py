0ae3fdaa6f838cfaec9afc40dd482a0f6ce5eddd
caipeichao <346089341@qq.com>
2012-09-18 17:08:10 +0800

    Add histo.bundle.mail

---
+++ Histo/histo/bundle/mail.py
@@ -0,0 +1,12 @@
+class Mail:
+    def __init__(self):
+        pass
+    
+    def open(self, name, mode):
+        pass
+    
+    def delete(self, name):
+        pass
+    
+    def list(self):
+        pass
