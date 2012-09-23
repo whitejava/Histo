5ba15d413166c4c9f214423194e55d1c8aa20fb6
caipeichao <346089341@qq.com>
2012-09-23 08:18:55 +0800

    Update test: histo.bundle.crypto

--- Histo/test/histo/bundle/crypto.py
+++ Histo/test/histo/bundle/crypto.py
@@ -1,31 +1,60 @@
 def main():
     bundle = Bundle()
-    testWrite(bundle)
-    testRead(bundle)
-    
-    
-    from histo.bundle import Local
+    size = testWrite(bundle)
+    testRead(bundle, size)
+
+def Bundle():
+    from histo.bundle import Local, Crypto
     from pclib import timetext
-    from histo.cipher import AES, Hub, Verify
-    key1 = b'1' * 32
-    key2 = b'2' * 32
-    cipher = Hub(Verify('md5'), AES(key2), Verify('md5'), AES(key1), Verify('md5'))
-    bundle = Crypto(Local('D:\\%s-test-Crypto' % timetext()), cipher)
-    size = 0
+    return Crypto(Local('D:\\%s-test-crypto' % timetext()), Cipher())
+
+def testWrite(bundle):
     with bundle.open('test', 'wb') as f:
-        for _ in range(10000):
-            import random
-            e = abs(random.gauss(0, 100000))
-            size += e
-            f.write('a' * e)
+        return testWrite2(f)
+
+def testRead(bundle, size):
     with bundle.open('test', 'rb') as f:
-        while True:
-            a = f.read(1000)
-            if not a:
-                break
-            assert a == 'a' * len(a)
-            size -= len(a)
-    assert size == 0
+        testRead2(f, size)
+
+def Cipher():
+    from histo.cipher import Hub
+    return Hub(Verify2(), Cipher2(), AES2(), Cipher2(), Verify2())
+
+def Cipher2():
+    from histo.cipher import Hub
+    return Hub(Verify2(), AES2(), Verify2(), AES2(), Verify2())
+
+def Verify2():
+    from histo.cipher import Verify
+    return Verify('md5')
+
+def AES2():
+    from histo.cipher import AES
+    return AES(randomKey())
+
+def testWrite2(file):
+    size = 0
+    for _ in range(10000):
+        import random
+        e = abs(int(random.gauss(16,16)))
+        size += e
+        file.write(b'a'*e)
+    return size
+
+def testRead2(file, size):
+    readSize = 0
+    while True:
+        import random
+        e = random.randrange(1,100)
+        read = file.read(e)
+        if not read:
+            break
+        readSize += len(read)
+    assert readSize == size
+
+def randomKey():
+    import random
+    return bytes([random.randrange(256) for _ in range(32)])
 
 if __name__ == '__main__':
     test()
